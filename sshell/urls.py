38b7b71af338216d922d256b6954d2d7dcb54721
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-06-17 15:52:26 +0530

    Updated requirements.txt

--- sshell/urls.py
+++ sshell/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django_email_verification import urls as mail_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
