b489cf51e15bba25452a8a615a098fb55e1e8532
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-08-10 14:21:16 +0530

    Static files root and directory renamed

--- sshell/settings.py
+++ sshell/settings.py
@@ -152,10 +152,10 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_URL = '/media/'
